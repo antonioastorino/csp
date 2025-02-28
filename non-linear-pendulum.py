45935058e4bbdf9560bc89c7b1de5511d793035a
Antonio Astorino <antonio.astorino.phd@gmail.com>
2025-02-28 13:18:13 +0100

    Add energy calculation and plot

--- non-linear-pendulum.py
+++ non-linear-pendulum.py
@@ -33,10 +33,12 @@ import matplotlib.pyplot as plt
 
 DURATION = 10  # s
 dt = 0.001  # s
-G = 9.81  # m / s^2
-L = 2  # m
+g = 9.81  # m / s^2
+l = 2  # m
+m = 1  # kg
 
 NUM_OF_SAMPLES = round(DURATION / dt)
+
 
 def myPlotter(xVals, yVals, xLabel, yLabel, title):
     plt.figure(figsize=(8, 6))
@@ -48,30 +50,41 @@ def myPlotter(xVals, yVals, xLabel, yLabel, title):
 
 
 def theta_t_plus_dt(dt, theta_t, theta_t_minus_dt):
-    return -G / L * np.sin(theta_t) * (dt * dt) - theta_t_minus_dt + 2 * theta_t
+    return -g / l * np.sin(theta_t) * (dt * dt) - theta_t_minus_dt + 2 * theta_t
 
 
 t = np.linspace(0, NUM_OF_SAMPLES * dt, NUM_OF_SAMPLES - 1)
 theta_curr = np.pi / 730
 theta_prev = 0
 theta_next = 0
-theta_saved = []
+theta_vec = [0 for _ in range(0, NUM_OF_SAMPLES - 1)]
+omega_vec = [0 for _ in range(0, NUM_OF_SAMPLES - 1)]
 for i in range(0, NUM_OF_SAMPLES - 1):
     theta_next = theta_t_plus_dt(dt, theta_curr, theta_prev)
+    omega_vec[i] = (theta_curr - theta_prev) / dt
     theta_prev = theta_curr
     theta_curr = theta_next
-    theta_saved.append(theta_curr)
+    theta_vec[i] = theta_curr
 
-myPlotter(t, theta_saved, 't [s]', 'theta [rad]', 'time vs angle')
+myPlotter(t, theta_vec, 't [s]', 'theta [rad]', 'time vs angle')
 
 fig, ax = plt.subplots()
-ax.set_xlim(-L * 1.1, L * 1.1)
-ax.set_ylim(-L * 1.1, L * 1.1)
+ax.set_xlim(-l * 1.1, l * 1.1)
+ax.set_ylim(-l * 1.1, l * 1.1)
 ax.set_aspect('equal', adjustable='box')
 position_plot, = ax.plot(1, 1, marker='*')
 refresh_rate = 50  # Hz
 undersampling_rate = round(1 / dt / refresh_rate)
 for i in range(0, NUM_OF_SAMPLES - 1, undersampling_rate):
-    position_plot.set_data([0, L * np.sin(theta_saved[i])], [0, - L * np.cos(theta_saved[i])])
+    position_plot.set_data([0, l * np.sin(theta_vec[i])], [0, - l * np.cos(theta_vec[i])])
     plt.pause(1 / refresh_rate)
+
+# Energy / state space
+myPlotter(theta_vec, omega_vec, 'theta', 'omega', 'State Space')
+KE = [(omega_vec[i] * l)**2 / 2 / m for i in range(0, NUM_OF_SAMPLES - 1)]
+U = [m * g * l * (1 - np.cos(theta_vec[i])) for i in range(0, NUM_OF_SAMPLES - 1)]
+E = [KE[i] + U[i] for i in range(0, NUM_OF_SAMPLES - 1)]
+myPlotter(KE, U, 'kinetic [J]', 'potential [J]', 'Energy')
+myPlotter(t, E, 'time [s]', 'total energy [J]', 'Energy vs time')
+
 plt.show()
